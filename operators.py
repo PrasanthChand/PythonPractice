bd4eb112afe39efa89d1ad98b8df28d1b0221d90
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-18 13:16:29 +0530

    adds operators

---
+++ operators.py
@@ -0,0 +1,24 @@
+# Operators
+
+# Arithmetic Operators
+a = 6
+b = 9
+print(a + b)
+print(a - b)
+print(a % b)
+
+# Assignment Operators
+c = 7
+
+# Comparison Operators
+print(a == b)
+print(a >= b)
+print(a != b)
+
+# Logical Operators
+print((a <= b) and (b <= c))
+print((a <= b) or (b <= c))
+
+# Identity Operators
+print(a is b)
+print(a is not b)
