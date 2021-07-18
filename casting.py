6593728223ef2fcaeeebda39a5f29b1440dca655
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-18 12:05:33 +0530

    adds casting

---
+++ casting.py
@@ -0,0 +1,16 @@
+# casting
+# Integer
+a = 5
+
+# casting Integer to String
+a1 = str(a)
+print(type(a))
+print(type(a1))
+
+# float
+c = 19.005
+
+# casting float to Integer
+c1 = int(c)
+print(type(c))
+print(type(c1))
