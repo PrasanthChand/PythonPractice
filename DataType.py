a7f3122894095621d3963de06daff17952a2a85b
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-18 12:01:18 +0530

    adds variable and dataType

---
+++ DataType.py
@@ -0,0 +1,21 @@
+# Data Types
+
+# integer
+a = 5
+print(type(a))
+
+# string
+b = 'hello'
+print(type(b))
+
+# float
+c = 0.005
+print(type(c))
+
+# complex
+d = 5+6j
+print(type(d))
+
+# boolean
+e = True
+print(type(e))
---
+++ variable.py
@@ -0,0 +1,24 @@
+# variable
+
+# integer
+a = 5
+
+# casting Integer to String
+a1 = str(5)
+print(a1)
+
+# string
+b = 'hello'
+
+# float
+c = 19.005
+
+# casting float to Integer
+c1 = int(c)
+print(c1)
+
+# complex
+d = 5+6j
+
+# boolean
+e = True
