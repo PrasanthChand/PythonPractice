1d44fdc5083b09f31429329a6f07ee35546670ed
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-20 15:02:11 +0530

    updates regular expresion

--- Regular Expression.py
+++ Regular Expression.py
@@ -1,6 +1,15 @@
 # Regular Expression
 
+import re
+
+pattern = '^a...s$'
+test_string = 'abyss'
+result = re.match(pattern, test_string)
+
+if result:
+    print("Search successful.")
+else:
+    print("Search unsuccessful.")
 
 
-string = 'Hey there, how are you ?'
 
