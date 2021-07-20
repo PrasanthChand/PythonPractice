14ff842a88a64706398f8ff552e8a47db5a14e53
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-20 14:39:33 +0530

    adds lambda

---
+++ lambda.py
@@ -0,0 +1,17 @@
+# lambda
+
+x = lambda a: a + 4
+
+print(x(3))
+
+# using two arguments
+
+y = lambda a, b: a * b
+
+print(y(2, 5))
+
+# triple a number using lambda
+
+z = lambda n: n * 3
+
+print(z(7))
