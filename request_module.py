9a89b45b9e47394ea25a93b74f434a7a974473d7
Chandrasekar <Prasanth.Chandrasekar@emc.com>
2021-07-22 19:25:54 +0530

    adds requests modules

---
+++ request_module.py
@@ -0,0 +1,18 @@
+# requests module
+
+import requests
+import json
+
+# Making a GET request
+r = requests.get('http://worldtimeapi.org/api/timezone/Asia/Kolkata')
+
+# check status code for response received
+# success code - 200
+print(r)
+
+# print content of request
+print(r.text)
+
+
+
+
